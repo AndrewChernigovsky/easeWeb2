e954944eebd1bd3516c3750526a0c478ce4d2e04
UserRF <you@example.com>
2022-10-04 16:24:21 +0300

    1

--- src/js/app.js
+++ src/js/app.js
@@ -19,37 +19,51 @@ import scrollSmooth from './components/scroll-smooth';
   });
 })(jQuery);
 
+
+// 1) Отслеживаем высоту документа, его скролл
+// window.addEventListener('scroll', function() {
+
+//   console.log('расстояние пролистывания ' + Math.floor(scrollY, 1) + 'px');
+//   const height = document.body.offsetHeight
+//   const screenHeight = window.innerHeight
+//   let elHeigth =  SHAPE.offsetHeight;
+//   let scrollYY = scrollY + screenHeight;
+//   let centerScreen = screenHeight / 2;
+//   let centerScreen1 = screenHeight / 2;
+//   let scrollbottom = document.body.scrollTop + c.bottom;
+
+//   console.log('top:' + Math.floor(scrollYY, 1) + ' bottom: ' + Math.floor(scrollbottom, 1));
+//   console.log('center:' + Math.floor(centerScreen1, 1));
+
+//   if(scrollYY < centerScreen && scrollYY >= c.top) {
+//     let x = scrollYY * 100% + '%';
+//     console.log(x)
+//   }
+//   let x = scrollYY;
+//   let y = ;
+//   let z = ;
+//   let o = ;
+//   if (scrollYY >= c.top) {
+//     SHAPE.style.borderRadius = `${x} ${y} ${z} ${o} / 100% 100% 0% 0%`;
+//   }
+// });
+
+ // let scrolltop = document.body.scrollTop + c.top;
+
+
 const SHAPE = document.getElementById('mainFormShape');
 
 
-// Отслеживаем изменение окна браузера
-window.addEventListener("resize", function() {
-  const windowInnerWidth  = document.documentElement.clientWidth;
-  const windowInnerHeight = document.documentElement.clientHeight;
-  console.log('width Browser ', windowInnerWidth, 'height Browser ', windowInnerHeight)
-}, false);
 
-const windowInnerWidth  =screen.width;
-const windowInnerHeight = document.documentElement.clientHeight;
-console.log('height Browser:' + windowInnerHeight)
+window.addEventListener('scroll', function(){
+    // 1 Вычислить всю высоту документа h
+    // 2 Вычислить всю высоту отображаемого окна h1
+    // 3 Вычислить высоту от вверха документа до вверхней границы блока t,h2
+    var coordinatsSHAPE = SHAPE.getBoundingClientRect();
+    console.log('Вычисляем координаты блока', coordinatsSHAPE)
+    // 4 Вычислить центр отображаемого окна относительно всего документа c
+    // 5 Вычислить рассотояние скроллинга
+    // 6 Если граница блока и низа отобржаемого окна совпадают, начать изменение блока в + иначе в -
 
-if(SHAPE) {
-  var c = SHAPE.getBoundingClientRect();
-}
-console.log('shape sizes:' + c.height)
-// 1) Отслеживаем высоту документа, его скролл
-window.addEventListener('scroll', function() {
-  // console.log(scrollY + 'px');
-  var c = SHAPE.getBoundingClientRect();
-  let elHeigth =  SHAPE.offsetHeight;
-  // let scrolltop = (document.body.scrollTop + c.top) + elHeigth;
-  let scrolltop = document.body.scrollTop + c.top;
-  let scrollleft = document.body.scrollLeft + c.left;
-  console.log('top:' + scrolltop + ' left: ' + scrollleft + '');
-});
 
-window.addEventListener('scroll', function() {
-
-});
-
-// 2) Отслеживаем положение блока Элемента
+})
--- src/js/app222.js
+++
@@ -1,6 +0,0 @@
-const SHAPE = document.getElementById('mainFormShape');
-
-const windowInnerWidth  = window.innerWidth;
-const windowInnerHeight = window.innerHeight;
-
-console.log("width", ':', windowInnerWidth+' '+"height", ':', windowInnerHeight)
