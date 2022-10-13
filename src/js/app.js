c0c6497dcb416d354ada0cce89f13d47def39d0e
UserRF <you@example.com>
2022-10-13 18:17:45 +0300

    1

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,12 @@ import swiperMain from './components/sliders/swiperMain';
 		swiperMain.init();
 
 		$(function() {
-      		$('.intro-section__texts-inner h2').each(function(i) {
+      		$('.intro-section__texts-inner .animaH2').each(function(i) {
+				for(let z=0; z<100; z++) {
+					$(this).delay((i++) * 1000).fadeTo(1000, 1).fadeTo(1000,0);
+				}
+			})
+      		$('.intro-section__texts-inner .anima').each(function(i) {
 				for(let z=0; z<100; z++) {
 					$(this).delay((i++) * 2000).fadeTo(1000, 1).fadeTo(1000,0);
 				}
@@ -29,9 +34,9 @@ import swiperMain from './components/sliders/swiperMain';
 	});
 })(jQuery);
 
+const BODY = document.querySelector("body");
+
 const SHAPE = document.getElementById('mainFormShape');
-
-
 
 const active = 'active';
 // const overlay = 'overlay';
@@ -43,110 +48,6 @@ const tarifWrapper = document.querySelectorAll('.prices-section__tarif-wrapper')
 const tarifTitle = document.querySelector('.prices-section__title');
 
 let coordinatsSHAPE = SHAPE.getBoundingClientRect();
-
-
-// window.addEventListener('scroll', function () {
-// 	// 1 Вычислить всю высоту документа h
-// 	let screenHeight = document.body.scrollHeight;
-// 	console.log('Высота всего документа h', Math.floor(screenHeight, 1) + ' px');
-
-// 	// 2 Вычислить всю высоту отображаемого окна h1
-// 	let windowInnerWidth = document.documentElement.clientWidth;
-// 	let windowInnerHeight = document.documentElement.clientHeight;
-
-// 	console.log('высота отображаемого окна h1', Math.floor(windowInnerHeight, 1) + ' px');
-
-// 	// 3 Вычислить высоту от вверха документа до вверхней границы блока t,h2
-// 	console.log('Вычисляем координаты блока', coordinatsSHAPE);
-
-// 	// 4 Вычислить центр отображаемого окна относительно всего документа c
-// 	let scrolltop1 = document.body.scrollTop + windowInnerHeight + scrollY;
-// 	let centerScreen = (scrolltop1 + scrollY) / 2;
-// 	console.log('Вычисляем центр:' + Math.floor(centerScreen, 1) + ' px');
-
-// 	// 5 Вычислить рассотояние скроллинга
-// 	console.log('Вычисляем рассотояние скроллинга ' + Math.floor(scrollY, 1) + ' px');
-
-// 	// 6 Вычисляем расстояние низа отображаемого документа до верхней границы блока
-// 	let scrolltop = document.body.scrollTop + windowInnerHeight + scrollY;
-// 	console.log('нижняя граница отображаемого окна:' + Math.floor(scrolltop, 1) + ' px');
-
-// 	// 7 Вычисляем расстояние от верха всего документа до верхней границы блока
-// 	let offsetTop = screenHeight - scrollY - coordinatsSHAPE.bottom + coordinatsSHAPE.height;
-// 	console.log('расстояние от верха всего документа до верхней границы блока:' + Math.floor(offsetTop, 1) + ' px');
-
-// 	// 8 Координата вверха блока
-// 	let topSHAPE = coordinatsSHAPE.top;
-// 	console.log('верхняя граница блока:' + Math.floor(topSHAPE, 1) + ' px');
-
-// 	// 9 Если граница блока и низа отобржаемого окна совпадают, начать изменение блока в + иначе в -
-// 	if (scrolltop >= topSHAPE) {
-// 		let s = (Math.floor(scrollY, 0) - 1500);
-// 		console.log(s);
-// 		let x = Math.floor(s / 100) + 80 + '%';
-// 		console.log(x);
-
-// 		if (topSHAPE >= centerScreen) {
-// 			SHAPE.style.borderRadius = `${x} ${x} 0% 0%`;
-// 		} else {
-// 			SHAPE.style.borderRadius = `${x1} ${x1} 0% 0%`;
-// 		}
-
-// 		// let y = (SHAPE.style.borderRadius = `100% 100% 0% 0%`);
-// 		let y = (SHAPE.style.borderRadius = `${x} ${x} 0% 0%`);
-// 		SHAPE.style.borderRadius = `${x} ${x} 0% 0%`;
-// 		console.log('centerrrr');
-// 		console.log('бордер-радиус', y);
-// 		console.log('loggg');
-// 	}
-// });
-
-const stepsWorking = document.getElementById('stepsWorking-sectionID');
-const stepsWorkingSlide1 = document.querySelector('.stepsWorking-section__wrapper1')
-let coordinatsstepsWorking = stepsWorking.getBoundingClientRect();
-let coordinatsstepsWorkingSlide1 = stepsWorkingSlide1.getBoundingClientRect();
-
-let scrollWidthAll = Math.max(
-	stepsWorking.scrollWidth, stepsWorking.scrollWidth,
-	stepsWorking.offsetWidth, stepsWorking.offsetWidth,
-	stepsWorking.clientWidth, stepsWorking.clientWidth
-)
-
-window.addEventListener('scroll', function () {
-	let windowInnerHeight = document.documentElement.clientHeight;
-	console.log('Отображаемая область', windowInnerHeight)
-
-	let screenHeight = document.body.scrollHeight;
-	console.log('Высота всего документа h', Math.floor(screenHeight, 1) + ' px');
-
-	console.log('Вычисляем координаты блока', coordinatsstepsWorking);
-
-	console.log('Вычисляем координаты слайда', coordinatsstepsWorkingSlide1);
-
-	let scrolltop = document.body.scrollTop + windowInnerHeight + scrollY;
-	console.log('нижняя граница отображаемого окна:' + Math.floor(scrolltop, 1) + ' px');
-
-	let topstepsWorking = coordinatsstepsWorking.top;
-	let widthstepsWorking = coordinatsstepsWorking.width;
-	let widthstepsWorkingSlide = coordinatsstepsWorkingSlide1.width;
-
-	console.log('верхняя граница блока:' + Math.floor(topstepsWorking, 1) + ' px');
-
-	console.log('Вычисляем рассотояние скроллинга ' + Math.floor(scrollY, 1) + ' px');
-
-	console.log('Вычисляем ширину всего блока ' + Math.floor(widthstepsWorking, 1) + ' px');
-	console.log('Вычисляем ширину всего слайда ' + Math.floor(widthstepsWorkingSlide, 1) + ' px');
-	console.log('Вычисляем всю ширину ' + Math.floor(widthstepsWorkingSlide * 2, 1) + ' px');
-
-	if (scrolltop >= topstepsWorking) {
-		widthstepsWorking.style.width = widthstepsWorkingSlide * 2
-	}
-
-	stepsWorking.addEventListener('scroll',()=> {
-		console.log('Вычисляем ширину всего блока ' + Math.floor(coordinatsstepsWorking.width, 1) + ' px');
-
-	})
-});
 
 document.addEventListener('DOMContentLoaded', () => {
 	menuBtn.addEventListener('click', () => {
@@ -186,3 +87,111 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 });
 
+let targetStepWorking = document.getElementById("stepsWorkingSectionWrapper1");
+
+targetStepWorking.addEventListener('wheel', event => {
+	const toLeft  = event.deltaY < 0 && targetStepWorking.scrollLeft > 0
+	const toRight = event.deltaY > 0 && targetStepWorking.scrollLeft < targetStepWorking.scrollWidth - targetStepWorking.clientWidth
+
+	if (toLeft || toRight) {
+	  event.preventDefault()
+	  targetStepWorking.scrollLeft += event.deltaY
+	}
+});
+
+const blockWrapperSteps = document.querySelector('.stepsWorking-section__wrapper1');
+const blocksStepsWorking = document.querySelectorAll('.stepsWorking-section__step-block');
+
+let lastBlockInner = blocksStepsWorking[blocksStepsWorking.length - 1];
+
+(function Function__lastBlockInnerID() {
+	lastBlockInner.setAttribute('id', 'stepsWorkingLastBlock')
+})();
+
+let lastBlockInnerID =  document.querySelector('#stepsWorkingLastBlock');
+
+let optionsOuter = {
+    root: null,
+    rootMargin: "0px 0px -20% 0px",
+    threshold: 1,
+};
+let optionsList = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1,
+};
+
+let optionsSHAPE = {
+    root: null,
+    rootMargin: "-10% 0px -40% 0px",
+    threshold: buildThresholdList()
+};
+
+const numSteps = 100.0;
+let boxElement;
+let prevRatio = 0.0;
+
+function buildThresholdList() {
+	let thresholds = [];
+	let numSteps = 100;
+
+	for (let i=0.1; i<=numSteps; i++) {
+	  let ratio = i/numSteps;
+	  thresholds.push(ratio);
+	}
+
+	thresholds.push(0);
+	return thresholds;
+}
+
+// функция обратного вызова
+let callbackOuter = function (entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log("пересек");
+            BODY.classList.add("stop-scroll");
+		}
+	});
+};
+
+let callbackList = function (entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log("пересек последний");
+            BODY.classList.remove("stop-scroll");
+        }
+    });
+};
+
+let callbackSHAPE = function (entries, observer) {
+	entries.forEach((entry) => {
+		let i = 100;
+		console.log(i--)
+		let y = entry.intersectionRatio * 100 + '%';
+		let y1 = (entry.intersectionRatio * i--) + '%';
+		console.log(y1, '123123132 6666666666666')
+
+		if (entry.intersectionRatio > prevRatio) {
+			entry.target.style.borderRadius = `${y1} ${y1} 0 0`;
+			console.log(entry.intersectionRatio, '123123222')
+			console.log(entry.target.style.borderRadius)
+		}
+		// if (entry.intersectionRatio < prevRatio) {
+		// 	entry.target.style.borderRadius = `'${y} ${y} 0 0`;
+		// 	console.log(entry.intersectionRatio, '123123222')
+		// 	console.log(entry.target.style.borderRadius)
+		// } else {
+		//   	entry.target.style.borderRadius = `${y1} ${y1} 0 0`;
+		// }
+
+		prevRatio = entry.intersectionRatio;
+	});
+};
+
+let observerOuter = new IntersectionObserver(callbackOuter, optionsOuter);
+let observerList = new IntersectionObserver(callbackList, optionsList);
+let observerSHAPE = new IntersectionObserver(callbackSHAPE, optionsSHAPE);
+
+observerOuter.observe(blockWrapperSteps);
+observerList.observe(lastBlockInnerID);
+observerSHAPE.observe(SHAPE);
