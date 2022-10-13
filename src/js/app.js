7a77b69b559230900b41b74bb7db490cda799801
UserRF <you@example.com>
2022-10-13 21:29:50 +0300

    1

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,110 @@ import swiperMain from './components/sliders/swiperMain';
 // import initSelects from './components/select';
 // import bntOFFER from './components/btnOffer';
 
+// let array = [
+// 	0.00,
+// 	0.01,
+// 	0.02,
+// 	0.03,
+// 	0.04,
+// 	0.05,
+// 	0.06,
+// 	0.07,
+// 	0.08,
+// 	0.09,
+// 	0.10,
+// 	0.11,
+// 	0.12,
+// 	0.13,
+// 	0.14,
+// 	0.15,
+// 	0.16,
+// 	0.17,
+// 	0.18,
+// 	0.19,
+// 	0.20,
+// 	0.21,
+// 	0.22,
+// 	0.23,
+// 	0.24,
+// 	0.25,
+// 	0.26,
+// 	0.27,
+// 	0.28,
+// 	0.29,
+// 	0.30,
+// 	0.31,
+// 	0.32,
+// 	0.33,
+// 	0.34,
+// 	0.35,
+// 	0.36,
+// 	0.37,
+// 	0.38,
+// 	0.39,
+// 	0.40,
+// 	0.41,
+// 	0.42,
+// 	0.43,
+// 	0.44,
+// 	0.45,
+// 	0.46,
+// 	0.47,
+// 	0.48,
+// 	0.49,
+// 	0.50,
+// 	0.51,
+// 	0.52,
+// 	0.53,
+// 	0.54,
+// 	0.55,
+// 	0.56,
+// 	0.57,
+// 	0.58,
+// 	0.59,
+// 	0.60,
+// 	0.61,
+// 	0.62,
+// 	0.63,
+// 	0.64,
+// 	0.65,
+// 	0.66,
+// 	0.67,
+// 	0.68,
+// 	0.69,
+// 	0.70,
+// 	0.71,
+// 	0.72,
+// 	0.73,
+// 	0.74,
+// 	0.75,
+// 	0.76,
+// 	0.77,
+// 	0.78,
+// 	0.79,
+// 	0.80,
+// 	0.81,
+// 	0.82,
+// 	0.83,
+// 	0.84,
+// 	0.85,
+// 	0.86,
+// 	0.87,
+// 	0.88,
+// 	0.89,
+// 	0.90,
+// 	0.91,
+// 	0.92,
+// 	0.93,
+// 	0.94,
+// 	0.95,
+// 	0.96,
+// 	0.97,
+// 	0.98,
+// 	0.99,
+// 	1.00
+// ]
+
 (($) => {
 	// When DOM is ready
 	$(() => {
@@ -96,6 +200,10 @@ targetStepWorking.addEventListener('wheel', event => {
 	if (toLeft || toRight) {
 	  event.preventDefault()
 	  targetStepWorking.scrollLeft += event.deltaY
+	} else event.preventDefault()
+
+	if (targetStepWorking.scrollLeft > targetStepWorking.scrollWidth) {
+		event.preventDefault()
 	}
 });
 
@@ -112,7 +220,7 @@ let lastBlockInnerID =  document.querySelector('#stepsWorkingLastBlock');
 
 let optionsOuter = {
     root: null,
-    rootMargin: "0px 0px -20% 0px",
+    rootMargin: "0% 0px 0% 0px",
     threshold: 1,
 };
 let optionsList = {
@@ -123,7 +231,7 @@ let optionsList = {
 
 let optionsSHAPE = {
     root: null,
-    rootMargin: "-10% 0px -40% 0px",
+    rootMargin: "0px",
     threshold: buildThresholdList()
 };
 
@@ -135,7 +243,7 @@ function buildThresholdList() {
 	let thresholds = [];
 	let numSteps = 100;
 
-	for (let i=0.1; i<=numSteps; i++) {
+	for (let i=0; i<=numSteps; ++i) {
 	  let ratio = i/numSteps;
 	  thresholds.push(ratio);
 	}
@@ -159,15 +267,17 @@ let callbackList = function (entries, observer) {
         if (entry.isIntersecting) {
             console.log("пересек последний");
             BODY.classList.remove("stop-scroll");
+			blockWrapperSteps.style.overflowX = 'hidden';
         }
     });
 };
 
+let i = 100;
+
 let callbackSHAPE = function (entries, observer) {
 	entries.forEach((entry) => {
-		let i = 100;
-		console.log(i--)
-		let y = entry.intersectionRatio * 100 + '%';
+		console.log(i, 'i')
+		// let y = entry.intersectionRatio * 100 + '%';
 		let y1 = (entry.intersectionRatio * i--) + '%';
 		console.log(y1, '123123132 6666666666666')
 
@@ -195,3 +305,5 @@ let observerSHAPE = new IntersectionObserver(callbackSHAPE, optionsSHAPE);
 observerOuter.observe(blockWrapperSteps);
 observerList.observe(lastBlockInnerID);
 observerSHAPE.observe(SHAPE);
+
+
