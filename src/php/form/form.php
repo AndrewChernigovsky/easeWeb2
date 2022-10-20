a614b581a80f158377919f38260c0dff2f61946a
UserRF <you@example.com>
2022-10-20 12:59:17 +0300

    1

--- src/php/form/form.php
+++ src/php/form/form.php
@@ -1,46 +1,20 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-//require 'phpmailer/PHPMailer.php';
-require 'phpmailer/PHPMailerAutoload.php';
-
-if (!empty($_POST['username'])) {
-
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-
-    $mail->SMTPDebug = 1;
-
-    $mail->Host = 'smtp.mail.ru';
-
-    $mail->SMTPAuth = true;
-    $mail->Username = 'easewebcompany@mail.ru'; // логин от вашей почты
-    $mail->Password = 'Sinkhr0fazatron'; // пароль от почтового ящика
-    $mail->SMTPSecure = 'SSL';
-    $mail->Port = '465';
-
-    $mail->CharSet = 'UTF-8';
-    $mail->From = 'easewebcompany@mail.ru';  // адрес почты, с которой идет отправка
-    $mail->FromName = 'Easeweb | сайты, который продают'; // имя отправителя
-    $mail->addAddress('easeweb@mail.ru', 'Easeweb');
-
-    $mail->isHTML(true);
-
-    $mail->Subject = $_POST['subject'];
-    $mail->Body = "Имя: {$_POST['username']}<br> Телефон: {$_POST['userphone']}<br> Сообщение: " . nl2br($_POST['body']);
-    $mail->AltBody = "Имя: {$_POST['username']}\r\n Телефон: {$_POST['userphone']}\r\n Сообщение: {$_POST['body']}";
-
-    //$mail->SMTPDebug = 1;
-
-    if ($mail->send()) {
-        $answer = '1';
-    } else {
-        $answer = '0';
-        echo 'Письмо не может быть отправлено. ';
-        echo 'Ошибка: ' . $mail->ErrorInfo;
-    }
-    die($answer);
-}
-
-?>
+$fio = $_POST['username'];
+$email = $_POST['userphone'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("easeweb@mail.ru", "Заявка с сайта", "ФИО: ". $fio.". E-mail: ". $email ,"From: wordpress@izosimov-andrew.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+		echo header('location: thank-you.html');
+} else {
+    echo "при отправке сообщения возникли ошибки";
+	ini_set('display_errors','On');
+	error_reporting('E_ALL');
+}?>
--- src/php/mail.php
+++ src/php/mail.php
@@ -84,3 +84,50 @@ if(!$mail->send()) {
     header('location: thank-you.html');
 }
 ?>
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+//require 'phpmailer/PHPMailer.php';
+require 'phpmailer/PHPMailerAutoload.php';
+
+if (!empty($_POST['username'])) {
+
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+
+    $mail->SMTPDebug = 1;
+
+    $mail->Host = 'smtp.mail.ru';
+
+    $mail->SMTPAuth = true;
+    $mail->Username = 'easewebcompany@mail.ru'; // логин от вашей почты
+    $mail->Password = 'Sinkhr0fazatron'; // пароль от почтового ящика
+    $mail->SMTPSecure = 'SSL';
+    $mail->Port = '465';
+
+    $mail->CharSet = 'UTF-8';
+    $mail->From = 'easewebcompany@mail.ru';  // адрес почты, с которой идет отправка
+    $mail->FromName = 'Easeweb | сайты, который продают'; // имя отправителя
+    $mail->addAddress('easeweb@mail.ru', 'Easeweb');
+
+    $mail->isHTML(true);
+
+    $mail->Subject = $_POST['subject'];
+    $mail->Body = "Имя: {$_POST['username']}<br> Телефон: {$_POST['userphone']}<br> Сообщение: " . nl2br($_POST['body']);
+    $mail->AltBody = "Имя: {$_POST['username']}\r\n Телефон: {$_POST['userphone']}\r\n Сообщение: {$_POST['body']}";
+
+    //$mail->SMTPDebug = 1;
+
+    if ($mail->send()) {
+        $answer = '1';
+		header('location: thank-you.html');
+    } else {
+        $answer = '0';
+        echo 'Письмо не может быть отправлено. ';
+        echo 'Ошибка: ' . $mail->ErrorInfo;
+    }
+    die($answer);
+}
+
+?>
