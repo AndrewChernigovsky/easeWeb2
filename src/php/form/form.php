d79448fe8b735d444d5032429dc9345f49f2d0e6
UserRF <you@example.com>
2022-10-20 17:37:42 +0300

    1

--- src/php/form/form.php
+++ src/php/form/form.php
@@ -1,20 +1,50 @@
 <?php
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
 $fio = $_POST['username'];
-$email = $_POST['userphone'];
+$phone = $_POST['userphone'];
 $fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
+$phone = htmlspecialchars($phone);
 $fio = urldecode($fio);
-$email = urldecode($email);
+$phone = urldecode($phone);
 $fio = trim($fio);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("easeweb@mail.ru", "Заявка с сайта", "ФИО: ". $fio.". E-mail: ". $email ,"From: wordpress@izosimov-andrew.ru \r\n"))
- {     echo "сообщение успешно отправлено";
-		echo header('location: thank-you.html');
+$phone = trim($phone);
+$token = '5667961004:AAE_j-IbnI30eLZPqx9ejXNKYEDO-N7-HRA';
+$chat_id = '-620731731';
+
+$mail->isHTML(true);
+$mail->setFrom('wordpress@izosimov-andrew.ru');
+$mail->addAddress('easeweb@mail.ru');
+$mail->addAddress('easewebcompany@gmail.com');
+$mail->Subject = 'Easeweb | Обратный звонок';
+$mail->Body    = '' . '<p style="color: #1662a8; font-size: 16px;"><strong>' . $fio . '</strong></p>' . "<p style='color: black; font-size: 24px;'>телефон<strong> \r\n" . '+'.$phone . '</strong></p>';
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+	echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
-    echo "при отправке сообщения возникли ошибки";
-	ini_set('display_errors','On');
-	error_reporting('E_ALL');
-}?>
+    header('location: thank-you.html');
+}
+
+
+$arr = array(
+	'Имя пользователя: ' => $fio,
+	'Телефон: ' => '+'.$phone
+);
+
+foreach($arr as $key => $value) {
+$txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+header('Location: thank-you.html');
+} else {
+echo "Error";
+}
+
+?>
